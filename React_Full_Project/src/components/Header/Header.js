43633c47e913455482a7d0814df574d3c13d5c49
Łukasz Holeczek <lukasz@holeczek.pl>
2017-02-11 23:57:29 +0100

    2.3.1

---
+++ React_Full_Project/src/components/Header/Header.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { Dropdown, DropdownMenu, DropdownItem } from 'reactstrap';
+
+class Header extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  sidebarToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-hidden');
+  }
+
+  mobileSidebarToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('sidebar-mobile-show');
+  }
+
+  asideToggle(e) {
+    e.preventDefault();
+    document.body.classList.toggle('aside-menu-hidden');
+  }
+
+  render() {
+    return (
+      <header className="app-header navbar">
+        <button className="navbar-toggler mobile-sidebar-toggler hidden-lg-up" onClick={this.mobileSidebarToggle} type="button">&#9776;</button>
+        <a className="navbar-brand" href="#"></a>
+        <ul className="nav navbar-nav hidden-md-down b-r-1">
+          <li className="nav-item">
+            <a className="nav-link navbar-toggler sidebar-toggler" onClick={this.sidebarToggle} href="#">&#9776;</a>
+          </li>
+        </ul>
+        <form className="form-inline float-left px-2 hidden-md-down">
+          <i className="fa fa-search"></i>
+          <input className="form-control" type="text" placeholder="Are you looking for something?"/>
+        </form>
+        <ul className="nav navbar-nav ml-auto">
+          <li className="nav-item hidden-md-down">
+            <a className="nav-link" href="#"><i className="icon-bell"></i><span className="badge badge-pill badge-danger">5</span></a>
+          </li>
+          <li className="nav-item hidden-md-down">
+            <a className="nav-link" href="#"><i className="icon-list"></i></a>
+          </li>
+          <li className="nav-item hidden-md-down">
+            <a className="nav-link" href="#"><i className="icon-location-pin"></i></a>
+          </li>
+          <li className="nav-item dropdown">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+
+              <a onClick={this.toggle} className="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded={this.state.dropdownOpen}>
+              <i className="icon-settings"></i>
+              <span className="badge badge-pill badge-danger">9</span>
+              </a>
+
+              <DropdownMenu className="dropdown-menu-right">
+                <DropdownItem header className="text-center"><strong>Account</strong></DropdownItem>
+
+                <DropdownItem><i className="fa fa-bell-o"></i> Updates<span className="badge badge-info">42</span></DropdownItem>
+                <DropdownItem><i className="fa fa-envelope-o"></i> Messages<span className="badge badge-success">42</span></DropdownItem>
+                <DropdownItem><i className="fa fa-tasks"></i> Tasks<span className="badge badge-danger">42</span></DropdownItem>
+                <DropdownItem><i className="fa fa-comments"></i> Comments<span className="badge badge-warning">42</span></DropdownItem>
+
+                <DropdownItem header className="text-center"><strong>Settings</strong></DropdownItem>
+
+                <DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
+                <DropdownItem><i className="fa fa-wrench"></i> Settings</DropdownItem>
+                <DropdownItem><i className="fa fa-usd"></i> Payments<span className="badge badge-default">42</span></DropdownItem>
+                <DropdownItem><i className="fa fa-file"></i> Projects<span className="badge badge-primary">42</span></DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem><i className="fa fa-shield"></i> Lock Account</DropdownItem>
+                <DropdownItem><i className="fa fa-lock"></i> Logout</DropdownItem>
+
+              </DropdownMenu>
+            </Dropdown>
+          </li>
+          <li className="nav-item hidden-md-down">
+            <a className="nav-link navbar-toggler aside-menu-toggler" onClick={this.asideToggle} href="#">&#9776;</a>
+          </li>
+        </ul>
+      </header>
+    )
+  }
+}
+
+export default Header;
